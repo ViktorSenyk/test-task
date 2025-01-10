e7b3821b31cb1eb13c68b3669715ea56e040bba3
Viktor Senyk <86928639+ViktorSenyk@users.noreply.github.com>
2025-01-10 13:41:31 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import React from "react";
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import store from './store';
 import UserInfo from './users/UserInfo';
 import SearchField from './users/SearchField';
-import Spinner from "./users/Spinner";
-import store from './store';
 
-const App = () => {
-    return (
-
-        <Provider store={store}>
-            <div className="page">
-                <UserInfo />
-                <SearchField />
-            </div>
-        </Provider>
-    );
+export default class App extends Component {
+  render = () => (
+    <Provider store={store}>
+      <div className="page">
+        <UserInfo />
+        <SearchField />
+      </div>
+    </Provider>
+  );
 }
-
-export default App;
