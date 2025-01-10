4b54ddb4b1bc8fad724e70794fa7e12f74de65d1
Viktor Senyk <86928639+ViktorSenyk@users.noreply.github.com>
2025-01-10 13:33:34 +0200

    Update SearchField.jsx

--- src/users/SearchField.jsx
+++ src/users/SearchField.jsx
@@ -1,56 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import * as usersActions from './users.actions';
 
 class SearchField extends Component {
-    state = {
-        userName: ''
-    }
+  state = {
+    inputValue: '',
+  };
 
-    onChange = event => {
-        this.setState({ userName: event.target.value })
-    }
+  handleUserSearch = () => this.props.fetchUserData(this.state.inputValue);
 
-    handleUserSearch = () => {
-        // this.props.showSpinner();
-        this.props.fetchUserData(this.state.userName);
-        // getUserData(this.state.userName)
-        //     .then(userData => {
-        //         this.props.userDataRecieved(userData);
-        //     })
-    }
-
-    render() {
-        return (
-            <div className="name-form">
-                <input
-                    type="text"
-                    className="name-form__input"
-                    value={this.state.userName}
-                    onChange={this.onChange}
-                />
-                <button
-                    className="name-form__btn btn"
-                    onClick={this.handleUserSearch}
-                >
-                    Show
-                </button>
-            </div>
-        )
-    }
+  render = () => (
+    <div className="name-form">
+      <input
+        type="text"
+        className="name-form__input"
+        value={this.state.inputValue}
+        onChange={e => this.setState({ inputValue: e.target.value })}
+      />
+      <button className="name-form__btn btn" onClick={this.handleUserSearch}>
+        Show
+      </button>
+    </div>
+  );
 }
 
 const mapDispatch = {
-    // showSpinner: usersActions.showSpinner,
-    // userDataRecieved: usersActions.userDataRecieved,
-    fetchUserData: usersActions.fetchUserData,
-}
-
-SearchField.propTypes = {
-    // showSpinner: PropTypes.func.isRequired,
-    // userDataRecieved: PropTypes.func.isRequired,
-    fetchUserData: PropTypes.func.isRequired,
-}
+  fetchUserData: usersActions.fetchUserData,
+};
 
 export default connect(null, mapDispatch)(SearchField);
