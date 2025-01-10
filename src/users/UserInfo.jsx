dba2cd98e0e142b7fdce31fc4d0cc04f1f7a5116
Viktor Senyk <86928639+ViktorSenyk@users.noreply.github.com>
2025-01-10 13:24:29 +0200

    Update UserInfo.jsx

--- src/users/UserInfo.jsx
+++ src/users/UserInfo.jsx
@@ -4,73 +4,38 @@ import { connect } from 'react-redux';
 import { isFetchingSelector, userDataSelector } from './users.selectors';
 import Spinner from './Spinner';
 
-
 const UserInfo = ({ userData, isFetching }) => {
-    if (isFetching) {
-        return <Spinner />
-    }
-    if (!userData) {
-        return null;
-    }
-    return (
-        <div className='user'>
-            <img
-                alt="User Avantar"
-                src={userData.avatar_url}
-                className='user_avatar'
-            />
-            <div className="user__info">
-                <span className="user__name">{userData.name}</span>
-                <span className="user__location">{userData.location}</span>
-            </div>
-        </div>
-    )
-}
+  if (isFetching) {
+    return <Spinner />;
+  }
+  if (!userData) {
+    return null;
+  }
+  return (
+    <div className="user">
+      <img alt="User Avatar" src={userData.avatar_url} className="user_avatar" />
+      <div className="user__info">
+        <span className="user__name">{userData.name}</span>
+        <span className="user__location">{userData.location}</span>
+      </div>
+    </div>
+  );
+};
 
 UserInfo.propTypes = {
-    isFetching: PropTypes.bool.isRequired,
-    userData: PropTypes.shape(),
-}
+  isFetching: PropTypes.bool.isRequired,
+  userData: PropTypes.shape(),
+};
 
 UserInfo.defaultValue = {
-    userData: null,
-}
+  userData: null,
+};
 
 const mapState = state => {
-    return {
-        isFetching: isFetchingSelector(state),
-        userData: userDataSelector(state),
-    }
-}
+  return {
+    isFetching: isFetchingSelector(state),
+    userData: userDataSelector(state),
+  };
+};
 
 export default connect(mapState)(UserInfo);
-
-// const UserInfo = ({ userData, isFetching }) => {
-//     if (isFetching) {
-//         return <Spinner />;
-//     }
-//     if (!userData) {
-//         return null;
-//     }
-//     return (
-//         <div className='user'>
-//             <img
-//                 alt="User Avantar"
-//                 src={userData.avatar_url}
-//                 className='user_avatar'
-//             />
-//             <div class="user__info">
-//                 <span class="user__name">Apple</span>
-//                 <span class="user__location">Cupertino, CA</span>
-//             </div>
-//         </div>
-//     )
-// }
-
-// const mapSate = state => {
-//     return {
-//         isFetching: isFetchingSelector(state),
-//     };
-// }
-
-// export default connect(mapSate)(UserInfo);
